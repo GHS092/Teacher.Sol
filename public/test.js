e44d0ceae9282214872cc1a1a4271990508c19b8
GHS092 <tiktokghs2@gmail.com>
2025-03-17 20:53:31 -0500

    Add files via upload

---
+++ public/test.js
@@ -0,0 +1,7 @@
+// Archivo de prueba para verificar la entrega de archivos estáticos
+console.log('El archivo test.js se ha cargado correctamente');
+
+function testFunction() {
+  console.log('La función testFunction se ha ejecutado correctamente');
+  return 'La función testFunction se ha ejecutado correctamente';
+} 
